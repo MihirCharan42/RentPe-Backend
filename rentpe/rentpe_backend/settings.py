9672bac6dd783a7aa8d4c1a78390f84087ef8343
MihirCharan42 <mihircharan4242@gmail.com>
2023-08-08 13:33:42 +0530

    static files not loading fix

--- rentpe/rentpe_backend/settings.py
+++ rentpe/rentpe_backend/settings.py
@@ -158,15 +158,14 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
+# Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
